088d0235f3821c3a4e4368c53841b7ba2ef40bd7
sidheswar_ghosh@infosys.com <sidheswar_ghosh@infosys.com>
2015-09-30 15:02:42 +0530

    modify

---
+++ Demo/Demo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            Math.Abs(0.7);
+            return 0;
+        }
+    }
+}
